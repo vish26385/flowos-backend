7d41f86f509a8245408cd5a6754bcdb2449d930f
Vishnu Pandit <vish26385@gmail.com>
2025-09-27 11:50:51 +0530

    Initial commit - backend structure

---
+++ Data/FlowOSContext.cs
@@ -0,0 +1,16 @@
+﻿using FlowOS.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Task = FlowOS.Api.Models.Task;
+
+namespace FlowOS.Api.Data
+{
+    public class FlowOSContext : DbContext
+    {
+        public FlowOSContext(DbContextOptions<FlowOSContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<DailyPlan> DailyPlans { get; set; }
+    }
+}
---
+++ Models/DailyPlan.cs
@@ -0,0 +1,11 @@
+﻿namespace FlowOS.Api.Models
+{
+    public class DailyPlan
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string PlanJson { get; set; } = ""; // AI-generated plan in JSON
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
